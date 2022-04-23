25798132730d0f563850e25d602d9a8ac44ee025
muradissa <muradissa00@gmail.com>
2022-04-23 16:49:06 +0300

    Edit and save button update design css

--- src/app/components/cardreview/cardreview.component.ts
+++ src/app/components/cardreview/cardreview.component.ts
@@ -10,11 +10,34 @@ export class CardreviewComponent implements OnInit {
   constructor() { }
   @Input()review!: any;
   @Output() deleteEvent = new EventEmitter();
-
+  
+  contentEditable: boolean = false;
+  contentDisaplayButtpn:string ="none";
+  saveBtn :any;
+  bntStyle: string='btn-change';
+  
   ngOnInit(): void {
+     
   }
+  
   delete(review: any){
     this.deleteEvent.emit(review);  
   }
 
+  edit(review: any){
+    this.bntStyle = 'btn-default';
+    this.contentEditable = true;
+    var saveBtn = (document.getElementById('save-button') as HTMLButtonElement);
+   // saveBtn.style.display = "inline";    
+  }
+  
+  save(review : any){ 
+    this.bntStyle = 'btn-change';
+    this.contentEditable = false; 
+    var saveBtn = (document.getElementById('save-button') as HTMLButtonElement );
+    //saveBtn.style.display = "none";
+    //this.contentDisaplayButtpn= "none";
+  }
+  
+  
 }
